4aa647f1584f0caefbf277e4d2ad050800a7bd8b
Thomas Latortue <tsl13b@gmail.com>
2018-06-12 21:17:42 -0400

    testing some changes

--- src/containers/game_grid.js
+++ src/containers/game_grid.js
@@ -2,18 +2,68 @@ import React, { Component } from 'react';
 //import { connect } from 'react-redux'
 
 export default class GameGrid extends Component { 
+
     render() {
         return (
-            <div>
-                <div>
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                </div>
-                <div>
-                    <span className="dot"></span>
-                    <span className="dot"></span>
-                </div>
-            </div>
+            <tbody>
+                <tr>
+                    <td id="0.0" className="vertex"></td>
+                    <td id="0.1" className="hline"></td>
+                    <td id="0.2" className="vertex"></td>
+                </tr>
+                <tr>
+                    <td id="1.0" className="vline"></td>
+                    <td id="1.1" className="cell"></td>
+                    <td id="1.2" className="vline"></td>
+                </tr>
+                <tr>
+                    <td id="2.0" className="vertex"></td>
+                    <td id="2.1" className="hline"></td>
+                    <td id="2.2" className="vertex"></td>
+                </tr>
+            </tbody>
+
+            // <div>
+            //     <div>
+            //         <div>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //         </div>
+            //         <div>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //         </div>
+            //     </div>
+            //     <div>
+            //         <div>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //         </div>
+            //     </div>
+            //     <div>
+            //         <div>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //             <circle className="dot"></circle>
+            //         </div>
+            //     </div>
+            // </div>
+
         );
     }
 }
